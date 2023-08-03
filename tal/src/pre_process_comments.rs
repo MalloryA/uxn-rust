d1b18077f2d2d3983e757689abc9c8b662052878
Mallory Adams <malloryadams@fastmail.com>
2023-08-03 17:27:04 -0400

    Add PreProcessComments iterator (35/64)

--- tal/src/pre_process_comments.rs
+++ tal/src/pre_process_comments.rs
@@ -1,16 +1,22 @@
 use crate::chunker::Chunk;
 use crate::error::Error;
+use crate::token::Token;
+use crate::token::TokenType;
 use std::io::BufRead;
 use std::io::Bytes;
 use std::io::Read;
 
 pub struct PreProcessComments<'a> {
     chunks: &'a mut dyn Iterator<Item = Result<Chunk, Error>>,
+    comment_start: Option<Chunk>,
 }
 
 impl PreProcessComments<'_> {
     pub fn new(chunks: &mut dyn Iterator<Item = Result<Chunk, Error>>) -> PreProcessComments {
-        PreProcessComments { chunks }
+        PreProcessComments {
+            chunks,
+            comment_start: None,
+        }
     }
 }
 
@@ -18,7 +24,40 @@ impl Iterator for PreProcessComments<'_> {
     type Item = Result<Chunk, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.chunks.next()
+        loop {
+            let next = self.chunks.next();
+
+            if let Some(Ok(chunk)) = next {
+                // If we're in comment mode then we only care if our next token is a CommentEnd
+                if self.comment_start.is_some() {
+                    if let Ok(token) = Token::from_chunk(chunk.clone()) {
+                        if token.token_type == TokenType::CommentEnd {
+                            self.comment_start = None;
+                        }
+                    }
+                    continue;
+                }
+
+                // If our chunk represents a CommentStart then drop us into comment mode and skip to
+                // the next iteration
+                if let Ok(token) = Token::from_chunk(chunk.clone()) {
+                    if token.token_type == TokenType::CommentStart {
+                        self.comment_start = Some(chunk);
+                        continue;
+                    }
+                }
+                return Some(Ok(chunk));
+            }
+
+            if next.is_none() && self.comment_start.is_some() {
+                return Some(Err(Error::new(
+                    "reached EOF without finding comment close".to_string(),
+                    self.comment_start.clone().unwrap(),
+                )));
+            }
+
+            return next;
+        }
     }
 }
 
