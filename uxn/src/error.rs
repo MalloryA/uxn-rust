d21fc8afb077e5057e9e37479fff290620fbe098
Mallory Adams <malloryadams@fastmail.com>
2023-08-16 08:24:40 -0400

    Remove the emulator

--- uxn/src/error.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, PartialEq)]
-pub enum Error {
-    Underflow = 0x01,
-    Overflow = 0x02,
-    DivisionByZero = 0x03,
-    FailedToLoadRom = 0x04,
-    EndOfExecution = 0x05,
-}
--- uxn/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod cpu;
-pub mod device_memory;
-pub mod error;
-pub mod memory;
-pub mod stack;
